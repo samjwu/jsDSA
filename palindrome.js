998b7fed766baa528e668c967b0e3701b72fda2e
samjwu <sjwu@ualberta.ca>
2023-12-17 11:32:43 -0700

    Add palindrome checker

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+function palindrome(str) {
+  let filtered = str.replace(/[^\w]|_/g, "").toLowerCase();
+
+  for (let i = 0; i < filtered.length / 2; i++) {
+    if (filtered[i] != filtered[filtered.length-1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
